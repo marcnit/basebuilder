fd4b90711ae42a23596bea9044ad1b6ae9c86be4
Marcelo Pereira <marcelopereira@corp.globo.com>
2015-07-17 12:05:31 -0300

    Adicionando debug no install do NodeJs

--- nodejs/install.pl
+++ nodejs/install.pl
@@ -1,11 +1,17 @@
+#!/usr/bin/perl
+
 use YAML::XS 'LoadFile';
 use Data::Dumper;
 
 my $current_path = $ENV{'CURRENT_DIR'};
 my $file = $ARGV[0] || die "É necessário passar o arquivo para executar";
+print "####### Lendo arquivo $file ######\n";
+
 my $config = LoadFile($file);
 
 for (keys %{$config->{dependencies}}) {
+    print "####### Instalando Pacote NPM $_ $config->{dependencies}->{$_} ######\n";
+
     if ($config->{dependencies}->{$_}) {
       system("cd $current_path && npm install $_\@$config->{dependencies}->{$_}");
     } else {
