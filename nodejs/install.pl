8a7b05a4525ff49e7a5ed4824fd75a5c3a4e7713
Marcelo Pereira <marcelopereira@corp.globo.com>
2015-07-17 11:57:05 -0300

    Change NodeJS deploy with perl against ruby

---
+++ nodejs/install.pl
@@ -0,0 +1,14 @@
+use YAML::XS 'LoadFile';
+use Data::Dumper;
+
+my $current_path = $ENV{'CURRENT_DIR'};
+my $file = $ARGV[0] || die "É necessário passar o arquivo para executar";
+my $config = LoadFile($file);
+
+for (keys %{$config->{dependencies}}) {
+    if ($config->{dependencies}->{$_}) {
+      system("cd $current_path && npm install $_\@$config->{dependencies}->{$_}");
+    } else {
+      system("cd $current_path && npm install $_");
+    }
+}
